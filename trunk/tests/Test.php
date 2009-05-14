9de3cad015d2dae373608443169bb5cd50ce4e39
pavel.tatarsky <pavel.tatarsky@133d911a-40b1-11de-b466-9154fc769079>
2009-05-14 19:22:42 +0000

    More impressive test application

--- trunk/tests/Test.php
+++ trunk/tests/Test.php
@@ -1,10 +1,15 @@
 <?php
 class Test {
 	private $i = 1;
+	private $initMemUsage;
+	public function __construct() {
+		$this->initMemUsage = memory_get_usage();
+	}
 	public function run($request) {
 		$num = $this->i++;
+		$delta = memory_get_usage() - $this->initMemUsage;
 		$request->setHeader('Content-type', 'text/html');
-		$request->write('Hello! Your request is:');
+		$request->write('pid: '.getmypid().', current child request number: '.$num.', current memory usage: '.memory_get_usage().', memory usage delta: '.$delta.', your request is:');
 		while($data = $request->read()) {
 			$request->write($data);
 		}
